06dfde8b556550a7754022d7353f01f365cf3e3e
IvanSimonov <ivansimvar@gmail.com>
2022-01-27 12:01:48 +0200

    HW-13

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-const arHw = [13, 28, 4, 15, 25, -10, 17, 27];
-const comparator = (a,b) => {
-    let res = a%2 - b%2;
-    if(res == 0){
-        res = a%2 == 0 ? a-b : b-a;
+const ar1 = [3, 15 ,-19 ,4 ,22 ,17 ,1 ,2];
+let str = '';
+function myForEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+            callback(array[i], i, array);
     }
-    return res;
 }
-arHw.sort(comparator);
-console.log(arHw);
-
-/************************ */
-function matrixTransp(matrix) {
+myForEach(ar1,n => str += '#' + n);
+console.log(str);
+/////////////////////////////////////////////
+function mapper2(n) {
+    return n + 10;
+}
+function myMap(array, callback) {
     const res = [];
-
-    for (let i = 0; i < matrix[0].length; i++) {
-            res.push([]);
-            for (let j = 0; j < matrix.length; j++) {
-                    res[i].push(matrix[j][i]);
-            }
+    function forEachCall(n, i, a) {
+            res.push(callback(n, i, a));
     }
+    myForEach(array, forEachCall);
     return res;
 }
-displayMatrix(matrixTransp([[1,2], [3,4], [5,6]]));
-function displayMatrix(matrix) {
-    for (let i = 0; i < matrix.length; i++) {
-            let row = '';
-            for (let j = 0; j < matrix[i].length; j++) {
-                    row = row + matrix[i][j] + '   '
-            }
-            console.log(row);
-    }  
-}
+const ar2 = myMap(ar1,mapper2);
+console.log(ar2);
