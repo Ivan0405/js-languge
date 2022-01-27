339e85d6ff079fe087d3329eb40b8cdef6b05456
IvanSimonov <ivansimvar@gmail.com>
2022-01-27 20:05:31 +0200

    CW-Object

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
-const ar1 = [3, 15 ,-19 ,4 ,22 ,17 ,1 ,2];
-let str = '';
-function myForEach(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-            callback(array[i], i, array);
-    }
+const person1 = {id: 123, name:'Moshe', addres: {sity: 'Lod', street: 'Sokolov'}};
+const person2 = {id: 123, name:'Moshe', addres: {sity: 'Lod', street: 'Sokolov'}};
+const person3 = person1;
+console.log(`"123" == 123 is ${"123" == 123}`);
+console.log(`"123" === 123 is ${"123" === 123}`);
+console.log(`person1 === person3 is ${person1 == person3}`);
+console.log(`person1 === person2 is ${person1 === person2}`);
+console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${JSON.stringify(person1) === 
+        JSON.stringify(person2)}`);
+console.log(JSON.stringify(person1));
+console.log(person1.toString());
+console.log(`name of person1 is ${person1.name}`);
+console.log(`person1 livs in city ${person1.addres.sity}`);
+Object.keys(person1).forEach(k => console.log(k));//array of object keys
+Object.values(person1).forEach(v => console.log(v))//array of the object values
+Object.entries(person1).forEach(e=>console.log(e));//array of arrays - [key,value]
+console.log(Object.entries(person1));
+function createAddres(city, street){
+    // {city: city, street: street} <=> {city, street}
+    return {city, street}
 }
-myForEach(ar1,n => str += '#' + n);
-console.log(str);
-/////////////////////////////////////////////
-// function mapper2(n) {
-//     return n + 10;
-// }
-// function myMap(array, callback) {
-//     const res = [];
-//     function forEachCall(n, i, a) {
-//             res.push(callback(n, i, a));
-//     }
-//     myForEach(array, forEachCall);
-//     return res;
-// }
-// const ar2 = myMap(ar1,mapper2);
-// console.log(ar2);
-/*********************************cw14 */
-const ar20 = [13,17,20,23,2,40];
-// const arEvenOdd = ar20.filter((n, i, a) => a.length % 2 == 0 ?
-// n % 2 == 0 : n % 2 == 1);
-// console.log(arEvenOdd);
-/*********************************** */
-let res = ar20.reduce((res, cur) => res + cur, 0);
-console.log(res)
-const max = ar20.reduce((max, cur)=>cur > max ? cur : max, ar20[0]);
-console.log(max);
-console.log(res);
-// reduce with no second argument
-res = ar20.reduce((res, cur) => res + cur);
-/******************************************HW 14 definition task1 */
-//TODO write myReduce(array, callback, initialResult) based on myForEach
-//callback - function with four  possible parameters: accumulated result,
-// current element, current index, reference to array
-// if the user call doesn't contain initialResult, then the first element of the array will be
-//considered as initial result (in that case iterating begins from the second element of the array)
-/********************************************************************* */
-
+function createPerson(id, name, addres){
+    return {id, name, addres}
+}
+const persons = [
+    createPerson(123,"Vasya",createAddres("Rehovot","Parshani")),
+    createPerson(124,"Olya",createAddres("Rehovot","Pr. Plaut")),
+    createPerson(125,"Tolya",createAddres("Tel-Aviv","Dizingoff")),
+]
+console.log(persons);
