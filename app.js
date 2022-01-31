cd9390e3d5a3e9597e228a60b48d6d534c03a470
IvanSimonov <ivansimvar@gmail.com>
2022-01-31 09:44:00 +0200

    HW14-part_1

--- app.js
+++ app.js
@@ -41,4 +41,46 @@ res = ar20.reduce((res, cur) => res + cur);
 // if the user call doesn't contain initialResult, then the first element of the array will be
 //considered as initial result (in that case iterating begins from the second element of the array)
 /********************************************************************* */
+function myFilter(array, callbackPredicate) {
+    const res = [];
+    function forEachCallback(n, i, a) {
+            if (callbackPredicate(n, i, a)) {
+                    res.push(n);
+            }
+    }
+    myForEach(array, forEachCallback);
+    // function forEachCallback(n, i, a) {
+    //         if (callbackPredicate(n, i, a)) {
+    //                 res.push(n);
+    //         }
+    // }
+    myForEach(array, (n, i, a) => callbackPredicate(n, i, a) && res.push(n) );
+    return res;
+}
+const ar20 = [13, 17, 20, 23, 2, 40,50];
+const arEvenOdd = myFilter(ar20,(n, _i, a) => a.length % 2 == 0 ?
+ n % 2 == 0 : n % 2 == 1);
+console.log(arEvenOdd);
+function myReduce(array, callbackReduce, initial) {
+        
+        if (initial === undefined) {
+                initial = array[0];
+                array = array.slice(1);
+        }
+        let res = initial;
+        // function forEachCallack(n, i, a) {
+        //          res = callbackReduce(res,n,i,a );       
+        // }
+        myForEach(array,(n, i, a) => res = callbackReduce(res, n, i, a));
+        return res;
+}
+let res = myReduce(ar20, (res, cur) => res + cur, 0);
+console.log(res)
+let max = myReduce(ar20,(max, cur)=>cur > max ? cur : max, ar20[0]);
+console.log(max);
 
+// reduce with no second argument
+res = myReduce(ar20,(res, cur) => res + cur);
+max = myReduce(ar20,(max, cur)=>cur > max ? cur : max);
+console.log(res);
+console.log(max);
